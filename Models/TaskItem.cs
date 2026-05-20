0f7255b6e71d156fd13bf77d411c576498a3af9d
Your Name <you@example.com>
2026-05-20 09:03:35 +0300

    TaskItem

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 
 namespace TaskPlaner.Models
 {
@@ -14,9 +12,9 @@ namespace TaskPlaner.Models
         private string _description;
         private DateTime _startDate;
         private DateTime _endDate;
-        private Priority priority;
-        private Status status;
-        private int progress;
+        private Priority _priority;
+        private Status _status;
+        private int _progress;
         private List<string> _predecessorIds;
 
         public string Id
@@ -32,26 +30,58 @@ namespace TaskPlaner.Models
         public string Description
         {
             get => _description;
-            set { _id = value; OnPropertyChanged(); }
+            set { _description = value; OnPropertyChanged(); }
         }
-        public enum Priority
+        public DateTime StartDate
         {
-            Low,
-            Medium,
-            High
+            get => _startDate;
+            set { _startDate = value; OnPropertyChanged(); }
         }
-        public enum Status
+        public DateTime EndDate
         {
-            Planned,
-            InProgress,
-            Completed
+            get => _endDate;
+            set { _endDate = value; OnPropertyChanged(); }
+        }
+        public Priority Priority
+        {
+            get => _priority;
+            set { _priority = value; OnPropertyChanged(); }
+        }
+        public Status Status
+        {
+            get => _status;
+            set { _status = value; OnPropertyChanged(); }
+        }
+        public int Progress
+        {
+            get => _progress;
+            set { _progress = value; OnPropertyChanged(); }
+        }
+        public List<string> PredecessorIds
+        {
+            get => _predecessorIds;
+            set { _predecessorIds = value; OnPropertyChanged(); }
         }
 
-        public event PropertyChangedEventHandler? PropertyChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
 
-        public void OnPropertyChanged()
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
-
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+    }
+
+    public enum Priority
+    {
+        Low,
+        Medium,
+        High
+    }
+
+    public enum Status
+    {
+        Planned,
+        InProgress,
+        Completed
     }
 }
